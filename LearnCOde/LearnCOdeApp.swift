63f60bb534ad27757abb19815b23b5d4f3ccb5f2
gebgeb101 <gabriella_us@hotmail.com>
2025-03-24 20:18:17 +0700

    Initial Commit

---
+++ LearnCOde/LearnCOdeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnCOdeApp.swift
+//  LearnCOde
+//
+//  Created by Gabriella Sengkey on 24/03/25.
+//
+
+import SwiftUI
+
+@main
+struct LearnCOdeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
